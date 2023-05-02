9bbba8f6fc6e8559220a1b589638cdda06991447
dakyungoh <dakyung.work@gmail.com>
2023-05-02 21:58:25 +0900

    feat: react-router 도입

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Lending Page...</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login Page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register Page</div>,
+  },
+  {
+    path: "/my-page",
+    element: <div>My Page</div>,
+  },
+  {
+    path: "/board",
+    element: <div>Board Page</div>,
+  },
+  {
+    path: "/article/:id",
+    element: <div>Article Page</div>,
+  },
+  {
+    path: "/write",
+    element: <div>Write Page</div>,
+  },
+]);
+
 function App() {
-  return <div className="App">안녕하세요. 빨간 망토입니다.</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
