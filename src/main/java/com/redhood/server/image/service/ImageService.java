25995949734fd63c2b386778ec721a627cffc084
skawlgns1 <dydcjsgid@gmail.com>
2023-05-05 10:36:29 +0900

    feat/image_init

--- src/main/java/com/redhood/server/image/service/ImageService.java
+++ src/main/java/com/redhood/server/image/service/ImageService.java
@@ -7,11 +7,9 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.Map;
 
 @Service
