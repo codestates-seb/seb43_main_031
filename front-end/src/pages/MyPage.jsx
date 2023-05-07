cfb03501ccf745570dd91766f0eeeb6c49e4f778
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-05-08 01:32:04 +0900

    feat: 유저 정보 get 요청 및 화면 정렬

--- front-end/src/pages/MyPage.jsx
+++ front-end/src/pages/MyPage.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import blankProfileImg from "../img/blank-profile.png";
+
+const Container = styled.div`
+  background-color: #fae7e7;
+  width: 100vw;
+`;
+
+const ProfileSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 720px;
+  margin: auto;
+  padding: calc(50px + 3%) 3%;
+`;
 
 function MyPage() {
+  // const [data, setData] = useState({});
+
+  // 아마 const memberId = userInfo.memberId 이런 식으로 로그인 시 저장해 둔 유저정보 받아올 듯
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await axios.get(`http://localhost:8080/members/${memberId}`);
+  //     setData(res.data);
+  //   };
+  //   fetchData();
+  // }, []);
+  const data = {
+    id: 1,
+    email: "test@gmail.com",
+    nickName: "nickname",
+    phone: "010-1234-5678",
+    image: "https://my-bucket.s3.ap-northeast-2.amazonaws.com/my-image.jpg",
+  };
+
   return (
-    <div>
-      <p>마이 페이지 입니당</p>
-    </div>
+    // 이미지 없을 때 응답 어떻게 오는 지 확인. get요청시 body에 정보를 담는 것이 맞는 지? image/Image 대소문자 체크
+    <Container>
+      <ProfileSection>
+        {data.image === "" ? (
+          <img className="profileImg" src={blankProfileImg} alt="blanked user profile image" />
+        ) : (
+          <img className="profileImg" src={data.image} alt="user profile image" />
+        )}
+        <span>{data.nickName}</span>
+        <span>{data.email}</span>
+        <span>{data.phone}</span>
+      </ProfileSection>
+    </Container>
   );
 }
 
