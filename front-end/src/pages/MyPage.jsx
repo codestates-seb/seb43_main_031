66ae01639bc54e3fe44e0a2d40cbc0605202de63
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-05-11 23:17:43 +0900

    style: 화면 크기별 프로필 CSS 구현

--- front-end/src/pages/MyPage.jsx
+++ front-end/src/pages/MyPage.jsx
@@ -1,82 +1,100 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import blankProfileImg from "../img/blank-profile.png";
 
-const Container = styled.div`
+import blankProfileImage from "../img/blank-profile.png";
+
+const EntireContainer = styled.div`
   width: 100vw;
   min-height: calc(100vh - 50px);
   padding: 5rem 0;
+  border: 1px solid red;
 `;
 
 const ProfileSection = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   max-width: 720px;
   margin: auto;
-  padding: 0 3rem;
+  padding: 0 2rem;
   gap: 3rem;
-  .profileImg {
+  border: 1px solid red;
+  @media (max-width: 500px) {
+    gap: 1.5rem;
+  }
+  > img {
     height: 20rem;
     width: 20rem;
+    border: 0.4rem solid #fff;
     border-radius: 70%;
     overflow: hidden;
-    border: 0.4rem solid #fff;
     @media (max-width: 500px) {
-      height: 15rem;
-      width: 15rem;
+      height: 10rem;
+      width: 10rem;
     }
-  }
-  @media (max-width: 500px) {
-    flex-direction: column;
   }
 `;
 
-const ProfileInfo = styled.div`
+const ProfileInformation = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 3rem;
   font-size: 1.5rem;
   @media (max-width: 500px) {
-    gap: 2.5rem;
+    gap: 1.5rem;
+    font-size: 1rem;
   }
   .nickName {
     font-size: 2rem;
     font-weight: 600;
+    @media (max-width: 500px) {
+      font-size: 1.5rem;
+      font-weight: 600;
+    }
   }
 `;
 
-const Buttons = styled.div`
+const ButtonContainer = styled.div`
   display: flex;
   gap: 2rem;
-  .btn {
+  @media (max-width: 500px) {
+    gap: 1rem;
+  }
+  > button {
     height: 40px;
     width: 120px;
-    border-radius: 20px;
-    font-weight: 600;
     border-style: none;
+    border-radius: 20px;
+    font-size: 1rem;
+    font-weight: 600;
     background-color: #fff;
     cursor: pointer;
+    @media (max-width: 500px) {
+      height: 25px;
+      width: 75px;
+      font-size: 0.8rem;
+    }
   }
-  .red {
+  .deleteButton {
     color: var(--error-color);
   }
 `;
 
-function MyPage() {
-  // const [data, setData] = useState({});
+export default function MyPage() {
+  // const [profile, setProfile] = useState({});
 
   // 아마 const memberId = user.memberId 이런 식으로 로그인 시 저장해 둔 유저정보 받아올 듯
   // useEffect(() => {
   //   const fetchData = async () => {
-  //     const res = await axios.get(`http://localhost:8080/members/${memberId}`);
-  //     setData(res.data);
+  //     const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/members/${memberId}`);
+  //     setProfile(res.data);
   //   };
   //   fetchData();
   // }, []);
-  const data = {
+
+  const profile = {
     memberId: 1,
     email: "test@gmail.com",
     nickName: "nickname",
@@ -84,30 +102,28 @@ function MyPage() {
     image: "",
   };
 
+  const { email, nickName, phone, image } = profile;
+
   return (
-    <Container>
+    <EntireContainer>
       <ProfileSection>
-        {data.image === "" || data.image === null ? (
-          <img className="profileImg" src={blankProfileImg} alt="blanked user profile" />
+        {image === "" || image === null ? (
+          <img src={blankProfileImage} alt="blanked user profile" />
         ) : (
-          <img className="profileImg" src={data.image} alt="user profile" />
+          <img src={image} alt="user profile" />
         )}
-        <ProfileInfo>
-          <span className="nickName">{data.nickName}</span>
-          <span>{data.email}</span>
-          <span>{data.phone}</span>
-          <Buttons>
-            <button className="btn" type="button">
-              프로필 수정하기
-            </button>
-            <button className="red btn" type="button">
+        <ProfileInformation>
+          <span className="nickName">{nickName}</span>
+          <span>{email}</span>
+          <span>{phone}</span>
+          <ButtonContainer>
+            <button type="button">수정하기</button>
+            <button className="deleteButton" type="button">
               회원탈퇴
             </button>
-          </Buttons>
-        </ProfileInfo>
+          </ButtonContainer>
+        </ProfileInformation>
       </ProfileSection>
-    </Container>
+    </EntireContainer>
   );
 }
-
-export default MyPage;
