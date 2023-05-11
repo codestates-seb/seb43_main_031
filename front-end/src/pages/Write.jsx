d61a364cebfcef5338a80f626fb024a8a9fd1a6e
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-05-11 15:21:12 +0900

    refactor: 코드정리, 리팩토링

--- front-end/src/pages/Write.jsx
+++ front-end/src/pages/Write.jsx
@@ -6,13 +6,10 @@ import { FaCommentDots, FaWonSign, FaMapPin } from "react-icons/fa";
 import { RxFileText } from "react-icons/rx";
 import { FiClock } from "react-icons/fi";
 import { Editor } from "@toast-ui/react-editor";
-import "@toast-ui/editor/dist/toastui-editor.css";
-import { guList, dongList } from "../data/SeoulDistricts";
 
-const Container = styled.div`
-  background-color: #fae7e7;
-  width: 100vw;
-`;
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { guList, dongList } from "../data/SeoulDistricts";
 
 const FormSection = styled.form`
   display: flex;
@@ -20,73 +17,72 @@ const FormSection = styled.form`
   max-width: 720px;
   margin: auto;
   padding: 3rem;
-  .label {
+  color: var(--font-color-bold);
+  > label {
+    margin-bottom: 3%;
     font-size: 1.1rem;
     font-weight: 550;
-    margin-bottom: 3%;
   }
-  .input {
-    border-style: none;
-    border-radius: 5px;
+  > input {
     height: 30px;
     margin-bottom: 8%;
+    border-style: none;
+    border-radius: 5px;
     padding: 0.5rem;
     &:focus {
-      border: 1px solid #120003;
+      border: 1px solid var(--font-color-bold);
       outline: 4px solid #f7eaea;
     }
   }
-  .editor {
-    margin-top: 8%;
-  }
+`;
+
+const EditorContainer = styled.div`
+  margin-bottom: 8%;
 `;
 
 const TagContainer = styled.div`
   margin-top: 2%;
   margin-bottom: 5%;
-  .tag {
-    width: 80px;
+  > select {
     height: 30px;
+    width: 78px;
+    margin-right: 10px;
     border-radius: 20px;
     padding: 0.5rem;
-    background-color: #bd181f;
-    border: 1px solid #bd181f;
     color: #fff;
     font-weight: 500;
-    margin-right: 10px;
+    background-color: var(--primary-color);
     cursor: pointer;
     &:focus {
-      border: 1px solid #120003;
+      border: 1px solid var(--font-color-bold);
       outline: 4px solid #f7eaea;
     }
   }
 `;
 
-const Buttons = styled.div`
+const ButtonContainer = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   margin: 12% 0 6%;
   gap: 5%;
-  .btn {
+  > button {
     height: 40px;
     width: 120px;
+    border-style: none;
     border-radius: 20px;
     font-size: 1.1rem;
     font-weight: 600;
-    border-style: none;
+    background-color: #e3e3e3;
     box-shadow: #12000358 0px 4px 8px;
     cursor: pointer;
     &:active {
       transform: translateY(2px);
     }
   }
-  .red {
-    background-color: #bd181f;
+  .postButton {
     color: #fff;
-  }
-  .gray {
-    background-color: #e3e3e3;
+    background-color: var(--primary-color);
   }
 `;
 
@@ -94,7 +90,7 @@ export default function Write() {
   const navigate = useNavigate();
   const editorRef = useRef();
 
-  const [data, setData] = useState({
+  const [board, setBoard] = useState({
     title: "",
     content: "",
     cost: "",
@@ -103,158 +99,175 @@ export default function Write() {
     guTag: "",
     detailAddress: "",
   });
-  // console.log(data);
 
-  // 중복 제출 방지용
   const [disabled, setDisabled] = useState(false);
 
-  const handleChange = e => {
-    if (e.target) {
-      const { name, value } = e.target;
-      setData(prev => ({ ...prev, [name]: value }));
-    } else {
-      const editorInstance = editorRef.current.getInstance();
-      setData(prev => ({ ...prev, content: editorInstance.getMarkdown() }));
-    }
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setBoard(previous => ({ ...previous, [name]: value }));
+  };
+
+  const handleEditorChange = () => {
+    const editorInstance = editorRef.current.getInstance();
+    setBoard(previous => ({ ...previous, content: editorInstance.getMarkdown() }));
   };
 
   // POST 요청 부분
-  const handleSubmit = async e => {
-    e.preventDefault();
+  const handleSubmit = async event => {
+    event.preventDefault();
     setDisabled(true);
+    // try-catch문이 지저분해 보여 잘 안 쓰려고 하는 경향이 있다. -> 그럼 어떻게??
     try {
-      await axios.post("http://localhost:8080/boards", data);
+      await axios.post(`${process.env.REACT_APP_BASE_URL}/boards`, board);
       navigate("/boards");
     } catch (error) {
       alert("게시글 등록에 실패했습니다.");
+    } finally {
+      setDisabled(false);
     }
-    setDisabled(false);
   };
 
-  const handleCancel = e => {
-    e.preventDefault();
+  const handleCancel = event => {
+    event.preventDefault();
     confirm("정말로 취소하시겠습니까?") ? navigate("/boards") : null;
   };
+
+  function onGuChange(value) {
+    setBoard(previous => ({ ...previous, guTag: value }));
+  }
+
+  function onDongChange(value) {
+    setBoard(previous => ({ ...previous, dongTag: value }));
+  }
 
   // 에디터 내 이미지 업로드 hooks 수정
   const uploadImages = async (blob, callback) => {
     const formData = new FormData();
     formData.append("file", blob);
     try {
-      const res = await axios.post("http://localhost:8080/images", formData);
-      // 응답받은 url을 넣어준다.
-      callback(res.data.image);
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/images`, formData);
+      callback(response.data.image);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return (
-    <Container>
-      <FormSection onSubmit={handleSubmit}>
-        <label className="label" htmlFor="title">
-          <FaCommentDots /> 제목
-        </label>
+  const labels = [
+    {
+      id: "title",
+      title: "제목",
+      icon: <FaCommentDots />,
+      children: <input id="title" type="text" name="title" value={board.title} onChange={handleChange} required />,
+    },
+    {
+      id: "editor",
+      title: "상세내용",
+      icon: <RxFileText />,
+      children: (
+        <EditorContainer>
+          <Editor
+            initialValue=" "
+            previewStyle="vertical"
+            height="350px"
+            initialEditType="wysiwyg"
+            useCommandShortcut={false}
+            language="ko-KR"
+            ref={editorRef}
+            onChange={handleEditorChange}
+            hooks={{
+              addImageBlobHook: uploadImages,
+            }}
+          />
+        </EditorContainer>
+      ),
+    },
+    {
+      id: "cost",
+      title: "수고비(원)",
+      icon: <FaWonSign />,
+      children: <input id="cost" type="number" name="cost" value={board.cost} onChange={handleChange} required />,
+    },
+    {
+      id: "expiredDate",
+      title: "만료일",
+      icon: <FiClock />,
+      children: (
         <input
-          className="input"
-          id="title"
-          type="text"
-          name="title"
-          value={data.title}
-          onChange={handleChange}
-          required
-        />
-        <span className="label">
-          <RxFileText /> 상세내용
-        </span>
-        <Editor
-          initialValue=" "
-          previewStyle="vertical"
-          height="350px"
-          initialEditType="wysiwyg"
-          useCommandShortcut={false}
-          language="ko-KR"
-          ref={editorRef}
-          onChange={handleChange}
-          hooks={{
-            addImageBlobHook: uploadImages,
-          }}
-        />
-        <label className="label editor" htmlFor="cost">
-          <FaWonSign /> 수고비(원)
-        </label>
-        <input
-          className="input"
-          id="cost"
-          type="number"
-          name="cost"
-          value={data.cost}
-          onChange={handleChange}
-          required
-        />
-        <label className="label" htmlFor="expiredDate">
-          <FiClock /> 만료일
-        </label>
-        <input
-          className="input"
           id="expiredDate"
           type="datetime-local"
           name="expiredDate"
-          value={data.expiredDate}
+          value={board.expiredDate}
           onChange={handleChange}
           required
         />
-        <label className="label" htmlFor="detail">
-          <FaMapPin /> 상세주소
-        </label>
-        <SelectGuDong setData={setData} />
-        <input
-          className="input"
-          id="detail"
-          type="text"
-          name="detailAddress"
-          value={data.detailAddress}
-          onChange={handleChange}
-        />
-        <Buttons>
-          <button className="red btn" type="submit" disabled={disabled}>
-            등록하기
-          </button>
-          <button className="gray btn" type="button" onClick={handleCancel}>
-            취소하기
-          </button>
-        </Buttons>
-      </FormSection>
-    </Container>
+      ),
+    },
+    {
+      id: "detail",
+      title: "상세주소",
+      icon: <FaMapPin />,
+      children: (
+        <>
+          <SelectGuDong onGuChange={value => onGuChange(value)} onDongChange={value => onDongChange(value)} />
+          <input id="detail" type="text" name="detailAddress" value={board.detailAddress} onChange={handleChange} />
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <FormSection onSubmit={handleSubmit}>
+      {labels.map(label => (
+        <>
+          <label htmlFor={label.id}>
+            {label.icon}
+            {label.title}
+          </label>
+          {label.children}
+        </>
+      ))}
+      <ButtonContainer>
+        <button className="postButton" type="submit" disabled={disabled}>
+          등록하기
+        </button>
+        <button type="button" onClick={handleCancel}>
+          취소하기
+        </button>
+      </ButtonContainer>
+    </FormSection>
   );
 }
 
-function SelectGuDong(props) {
+function SelectGuDong({ onGuChange, onDongChange }) {
   const [selectedGu, setSelectedGu] = useState("");
   const [selectedDong, setSelectedDong] = useState("");
 
-  function handleGuChange(e) {
-    setSelectedGu(e.target.value);
-    props.setData(prev => ({ ...prev, guTag: e.target.value }));
+  function handleGuChange(event) {
+    setSelectedGu(event.target.value);
+    onGuChange(event.target.value);
   }
 
-  function handleDongChange(e) {
-    setSelectedDong(e.target.value);
-    props.setData(prev => ({ ...prev, dongTag: e.target.value }));
+  function handleDongChange(event) {
+    setSelectedDong(event.target.value);
+    onDongChange(event.target.value);
   }
 
   return (
     <TagContainer>
-      <select className="tag" value={selectedGu} onChange={handleGuChange} required>
-        {selectedGu === "" ? <option value="">지역구</option> : null}
+      <select value={selectedGu} onChange={handleGuChange} required>
+        <option value="" hidden>
+          지역구
+        </option>
         {guList.map(gu => (
           <option key={gu} value={gu}>
             {gu}
           </option>
         ))}
       </select>
-      <select className="tag" value={selectedDong} onChange={handleDongChange} disabled={!selectedGu} required>
-        {selectedDong === "" ? <option value="">지역동</option> : null}
+      <select value={selectedDong} onChange={handleDongChange} disabled={!selectedGu} required>
+        <option value="" hidden>
+          지역동
+        </option>
         {selectedGu &&
           dongList[selectedGu].map(dong => (
             <option key={dong} value={dong}>
