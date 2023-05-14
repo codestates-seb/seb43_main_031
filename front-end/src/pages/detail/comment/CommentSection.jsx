b7859dd1533b433a0748df9849c271a745d8f663
Suam Kang <ksa199653@gmail.com>
2023-05-15 01:45:43 +0900

    fix: 댓글/대댓글 수정기능 버그 수정

--- front-end/src/pages/detail/comment/CommentSection.jsx
+++ front-end/src/pages/detail/comment/CommentSection.jsx
@@ -9,7 +9,7 @@ import styled from "styled-components";
 
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 
-import { addComment, editComment, deleteComment } from "../../../redux/features/commentSlice";
+import { addComment, editComment, deleteComment, setComment } from "../../../redux/features/commentSlice";
 import ReplyCommentSection from "./ReplyCommentSection";
 import UserBox from "../UserBox";
 
@@ -31,35 +31,6 @@ const StyledListBlock = styled.div`
   margin-bottom: 0.5rem;
   border-radius: 0.4rem;
   box-shadow: 0 0.2rem 0.7rem 0.2rem rgba(0, 0, 0, 0.1);
-
-  .msg {
-    margin: 0 1rem 2rem;
-    font-size: 1.2rem;
-    font-weight: 500;
-  }
-`;
-
-// 댓글 아이템
-const StyledItemContents = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 1.2rem;
-
-  .fixAndDelete {
-    display: flex;
-    justify-content: center;
-    & > button {
-      margin: 0.3rem;
-      color: var(--sub-btn-color);
-      font-size: 0.8rem;
-      background-color: transparent; // 투명하게
-      border: none;
-      cursor: pointer;
-      &:hover {
-        color: var(--primary-color);
-      }
-    }
-  }
 `;
 
 // 새로운 글 작성 박스
@@ -71,7 +42,7 @@ const DetailSubForm = styled.div`
   flex-direction: column;
   gap: 1.3rem;
 
-  input {
+  & > input {
     width: 100%;
     height: 4rem;
     padding: 0 1rem 1rem;
@@ -80,7 +51,7 @@ const DetailSubForm = styled.div`
     background-color: var(--bg-color);
     font-size: 1.1rem;
   }
-  button {
+  & > button {
     width: 20%;
     align-self: end;
     padding: 0.7rem;
@@ -96,14 +67,72 @@ const DetailSubForm = styled.div`
   }
 `;
 
+// 댓글 아이템
+const StyledItemContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 1.2rem;
+
+  .msg {
+    margin: 1rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+`;
+
+// 수정할 폼 박스
+const EditForm = styled.div`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  & > input {
+    width: 100%;
+    height: 3rem;
+    padding: 0 0.5em 0.5rem;
+    border: none;
+    border-radius: 0.4rem;
+    background-color: var(--bg-color);
+    font-size: 1.1rem;
+  }
+
+  .utils {
+    display: flex;
+    align-self: end;
+    margin-top: 1rem;
+
+    .fixAndDelete {
+      display: flex;
+      justify-content: center;
+      & > button {
+        margin: 0.3rem;
+        color: var(--sub-btn-color);
+        font-size: 0.8rem;
+        background-color: transparent; // 투명하게
+        border: none;
+        cursor: pointer;
+        &:hover {
+          color: var(--primary-color);
+        }
+      }
+    }
+  }
+`;
+
 // 댓글 컴포넌트
 // 댓글과 대댓글 연결을 위한 상태(responseTo)를 추가하여 "root" 는 댓글, "responseTo"는 대댓글로 인식하게 했다.
-function CommentSection({ commentsData, boardData }) {
+function CommentSection() {
   // 왜 초기값인 boardInComments가 local상태로 안들어 가질까?
   // boardInComments는 있을수도 있고 없을수도 있는걸 유념해야한다.
-  const boardInComments = commentsData?.filter(comment => comment.boardId === boardData.id) || []; // 해당 게시판에 속한 코멘츠들만 불러오기(초기값)
+  // const boardInComments = commentData?.filter(comment => comment.boardId === boardData.id) || []; // 해당 게시판에 속한 코멘츠들만 불러오기(초기값)
 
-  // console.log(boardInComments);
+  const [local, setLocal] = useState([]); // 코멘츠 데이터들 배열 상태
+  const [text, setText] = useState(""); // 댓글 인풋 상태
+  const [editText, setEditText] = useState(""); // 댓글 수정창 인풋 상태
+  const [edit, setEdit] = useState(false); // 댓글 수정버튼 토글 상태
   const [user, setUser] = useState({
     memberId: 1,
     email: "test@gmail.com",
@@ -111,44 +140,62 @@ function CommentSection({ commentsData, boardData }) {
     phone: "010-1234-5678",
     image: "",
   });
-  const [local, setLocal] = useState([]); // 코멘츠 데이터들 배열 상태
-  const [text, setText] = useState(""); // 댓글 인풋 값 상태
-  // console.log(local);
+  const [openEditor, setOpenEditor] = useState(""); // 댓글 고유id값 담는 상태
+
   const { id } = useParams();
-  const comments = useSelector(state => state.comment);
   const dispatch = useDispatch();
-  const inputRef = useRef();
+  const comments = useSelector(state => state.comment);
+
+  const inputRef = useRef(null);
 
   // useEffect안에 적용하는 deps는 원시값처럼 간단한 상태를 적용해야 올바르게 돌아갈 수 있다.(공장처럼 코드를 수거해가서 대신 수행하는 느낌) ***
   useEffect(() => {
-    localStorage.setItem("comments", JSON.stringify(comments)); // 마운트 시 새로운 코멘츠 데이터 로컬스토리지에 저장
     setLocal(comments.filter(comment => comment.responseTo === "root")); // 코멘트(댓글)만 필터링해서 코멘츠 데이터 업데이트
   }, [comments]);
 
   // 새로운 댓글 생성
   const handleSubmit = e => {
     e.preventDefault();
-    const newData = {
-      boardId: id, // params로 받은 id
-      memberId: user.memberId,
-      commentId: uuid(), // 고유 아이디값 생성을 위한 리엑트 ID라이브러리 사용
-      content: text,
-      responseTo: "root", // 대댓글 연결을 위한 상태 추가
-      createdDate: `${new Date()}`,
-    };
-    dispatch(addComment(newData));
-    setText("");
+    if (text === "") {
+      alert("댓글을 작성해 주세요");
+    } else {
+      const newData = {
+        boardId: id, // params로 받은 id
+        memberId: user.memberId,
+        commentId: uuid(), // 고유 아이디값 생성을 위한 리엑트 ID라이브러리 사용
+        content: text,
+        responseTo: "root", // 대댓글 연결을 위한 상태 추가
+        createdDate: `${new Date()}`,
+        isEditing: false,
+      };
+      dispatch(addComment(newData));
+      setText("");
+    }
   };
 
   // 댓글 수정하기
-  const handleEdit = (commentId, text) => {
-    const editData = { commentId, content: text };
+  const handleEdit = (id, editText) => {
+    const editData = { commentId: id, content: editText };
     dispatch(editComment(editData));
   };
 
   // 댓글 삭제하기
-  const handleDelete = commentId => {
-    dispatch(deleteComment(commentId));
+  const handleDelete = id => {
+    dispatch(deleteComment(id));
+  };
+
+  // 수정버튼 클릭시 수정가능한 코멘트만 수정창 오픈
+  const handleEditClick = commentId => {
+    setEdit(!edit); // 수정 버튼 토글
+    setLocal(
+      comments.map(comment => {
+        if (comment.commentId === commentId) {
+          return { ...comment, isEditing: true };
+        } else {
+          return { ...comment, isEditing: false };
+        }
+      })
+    );
   };
 
   return (
@@ -170,18 +217,44 @@ function CommentSection({ commentsData, boardData }) {
           <StyledListBlock key={comment.commentId}>
             <StyledItemContents>
               <UserBox infoData={comment} />
-              {user.memberId === comment.memberId ? (
-                <div className="fixAndDelete">
-                  <button type="button" onClick={() => handleEdit(comment.commentId, text)}>
-                    수정
-                  </button>
-                  <button type="button" onClick={() => handleDelete(comment.commentId)}>
-                    삭제
-                  </button>
-                </div>
-              ) : null}
+              <div className="msg">{comment.content}</div>
+              {/* 유저가 해당 댓글의 작성자일 경우만 */}
+              {user.memberId === comment.memberId && (
+                // 해당 댓글/대댓글 id가 빈문자가 아니면 수정버튼 클릭시 수정인풋창 보이게
+                <EditForm>
+                  {openEditor === comment.commentId && (
+                    <input
+                      ref={inputRef}
+                      type="text"
+                      defaultValue={comment.content}
+                      key={comment.content}
+                      onChange={e => setEditText(e.target.value)}
+                    />
+                  )}
+                  <div className="utils">
+                    <div className="fixAndDelete">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          // 수정버튼 클릭시 일치하는 댓글이면 수정하고 아니면 id문자만 등록하여 인풋창 유지만 할 수 있게
+                          if (comment.commentId === openEditor) {
+                            handleEdit(comment.commentId, editText);
+                            setOpenEditor("");
+                          } else {
+                            setOpenEditor(comment.commentId);
+                          }
+                        }}
+                      >
+                        수정
+                      </button>
+                      <button type="button" onClick={() => handleDelete(comment.commentId)}>
+                        삭제
+                      </button>
+                    </div>
+                  </div>
+                </EditForm>
+              )}
             </StyledItemContents>
-            <div className="msg">{comment.content}</div>
             {/* 코멘트(부모)의 고유 아이디를 대댓글에서 저장할수 있도록 props전달  */}
             <ReplyCommentSection responseTo={comment.commentId} />
           </StyledListBlock>
--- front-end/src/pages/detail/comment/ReplyCommentSection.jsx
+++ front-end/src/pages/detail/comment/ReplyCommentSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import uuid from "react-uuid";
 
 import UserBox from "../UserBox";
-import { addComment } from "../../../redux/features/commentSlice";
+import { addComment, editComment, deleteComment } from "../../../redux/features/commentSlice";
 
 // 대댓글 컨테이너
 const ReplyContainer = styled.div`
@@ -68,15 +68,44 @@ const ReplyFormBox = styled.div`
 
 // 대댓글 아이템
 const ReplyItem = styled.div`
-  margin: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 1.2rem;
   padding: 0.5rem;
   border-bottom: 1px solid var(--primary-color);
 
+  .reply-msg {
+    margin: 1rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+`;
+
+// 수정할 폼 박스
+const EditForm = styled.div`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  & > input {
+    width: 100%;
+    height: 3rem;
+    padding: 0 0.5em 0.5rem;
+    border: none;
+    border-radius: 0.4rem;
+    background-color: var(--bg-color);
+    font-size: 1.1rem;
+  }
+
   .utils {
     display: flex;
-    justify-content: space-between;
+    align-self: end;
+    margin-top: 1rem;
 
-    & > .fixAndDelete {
+    .fixAndDelete {
       display: flex;
       justify-content: center;
       & > button {
@@ -92,17 +121,13 @@ const ReplyItem = styled.div`
       }
     }
   }
-
-  .reply-msg {
-    margin: 1rem;
-    padding: 1rem;
-  }
 `;
 
 function ReplyCommentSection({ responseTo }) {
   const [display, setDisplay] = useState(false); // 대댓글 폼 박스 활성화 상태
-  const [local, setLocal] = useState([]);
-  const [text, setText] = useState("");
+  const [local, setLocal] = useState([]); // 대댓글 배열 상태
+  const [text, setText] = useState(""); // 대댓글 인풋 상태
+  const [editText, setEditText] = useState(""); // 댓글 수정창 인풋 상태
   const [user, setUser] = useState({
     memberId: 1,
     email: "test@gmail.com",
@@ -110,10 +135,13 @@ function ReplyCommentSection({ responseTo }) {
     phone: "010-1234-5678",
     image: "",
   });
+  const [openEditor, setOpenEditor] = useState(""); // 댓글 고유id값 담는 상태
 
   const { id } = useParams();
   const dispatch = useDispatch();
   const comments = useSelector(state => state.comment);
+
+  const inputRef = useRef(null);
 
   useEffect(() => {
     localStorage.setItem("comments", JSON.stringify(comments));
@@ -123,15 +151,32 @@ function ReplyCommentSection({ responseTo }) {
   // 새로운 대댓글 작성
   const handleSubmit = e => {
     e.preventDefault();
-    const newData = {
-      boardId: id, // params로 받은 id
-      commentId: uuid(), // 고유 아이디값 생성을 위한 리엑트 ID라이브러리 사용
-      content: text,
-      responseTo, // 대댓글 연결을 위한 상태 추가
-      createdDate: `${new Date()}`,
-    };
-    dispatch(addComment(newData));
-    setText("");
+    if (text === "") {
+      alert("대댓글을 작성해 주세요");
+    } else {
+      const newData = {
+        boardId: id, // params로 받은 id
+        memberId: user.memberId,
+        commentId: uuid(), // 고유 아이디값 생성을 위한 리엑트 ID라이브러리 사용
+        content: text,
+        responseTo, // 대댓글 연결을 위한 상태 추가
+        createdDate: `${new Date()}`,
+        isEditing: false,
+      };
+      dispatch(addComment(newData));
+      setText("");
+    }
+  };
+
+  // 댓글 수정하기
+  const handleEdit = (id, editText) => {
+    const editData = { commentId: id, content: editText };
+    dispatch(editComment(editData));
+  };
+
+  // 댓글 삭제하기
+  const handleDelete = id => {
+    dispatch(deleteComment(id));
   };
 
   return (
@@ -150,16 +195,44 @@ function ReplyCommentSection({ responseTo }) {
         <ReplyFormBox>
           {local?.map(comment => (
             <ReplyItem key={comment.commnetId}>
-              <div className="utils">
-                <UserBox infoData={comment} />
-                {user === comment.memberId && (
-                  <div className="fixAndDelete">
-                    <button type="button">수정</button>
-                    <button type="button">삭제</button>
-                  </div>
-                )}
-              </div>
+              <UserBox infoData={comment} />
               <div className="reply-msg">{comment.content}</div>
+              {/* 유저가 해당 댓글의 작성자일 경우만 */}
+              {user.memberId === comment.memberId && (
+                // 해당 댓글/대댓글 id가 빈문자가 아니면 수정버튼 클릭시 수정인풋창 보이게
+                <EditForm>
+                  {openEditor === comment.commentId && (
+                    <input
+                      ref={inputRef}
+                      type="text"
+                      defaultValue={comment.content}
+                      key={comment.content}
+                      onChange={e => setEditText(e.target.value)}
+                    />
+                  )}
+                  <div className="utils">
+                    <div className="fixAndDelete">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          // 수정버튼 클릭시 일치하는 댓글이면 수정하고 아니면 id문자만 등록하여 인풋창 유지만 할 수 있게
+                          if (comment.commentId === openEditor) {
+                            handleEdit(comment.commentId, editText);
+                            setOpenEditor("");
+                          } else {
+                            setOpenEditor(comment.commentId);
+                          }
+                        }}
+                      >
+                        수정
+                      </button>
+                      <button type="button" onClick={() => handleDelete(comment.commentId)}>
+                        삭제
+                      </button>
+                    </div>
+                  </div>
+                </EditForm>
+              )}
             </ReplyItem>
           ))}
           <input
@@ -168,13 +241,12 @@ function ReplyCommentSection({ responseTo }) {
             value={text}
             placeholder="댓글추가"
             onChange={e => setText(e.target.value)}
-            onClick={handleSubmit}
           />
           <div className="utils">
             <button type="button" className="cancle" onClick={() => setDisplay(!display)}>
               취소
             </button>
-            <button type="button" className="add">
+            <button type="button" className="add" onClick={handleSubmit}>
               답글달기
             </button>
           </div>
