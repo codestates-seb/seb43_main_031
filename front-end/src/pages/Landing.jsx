653f1aa0af08cb442d1be8ff97a15070df6a774e
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-05-16 11:33:12 +0900

    feat: 스크롤 이벤트 추가

--- front-end/src/pages/Landing.jsx
+++ front-end/src/pages/Landing.jsx
@@ -1,10 +1,48 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
 import Footer from "../layouts/Footer";
 
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 50px);
+  font-size: 24px;
+  background-color: ${props => props.backgroundColor};
+`;
+
 export default function Landing() {
+  const [backgroundColor, setBackgroundColor] = useState("#FAE7E7");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      const windowHeight = window.innerHeight;
+
+      if (scrollTop > windowHeight / 2) {
+        setBackgroundColor("#DB8787");
+      } else {
+        setBackgroundColor("#FAE7E7");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
-      <p>랜딩 페이지 입니당</p>
+    <>
+      <StyledSection backgroundColor={backgroundColor}>
+        <h1>Landing Page</h1>
+      </StyledSection>
+      <StyledSection id="section2" backgroundColor="#DB8787">
+        <h1>Section 2</h1>
+      </StyledSection>
       <Footer />
-    </div>
+    </>
   );
 }
