451dfb73dca09d13f58a154d55afc055816b1f06
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-05-21 00:43:17 +0900

    fix: 휴대폰 번호 형식 변경 함수 수정

--- front-end/src/utils/formatPhoneNumber.js
+++ front-end/src/utils/formatPhoneNumber.js
@@ -1,5 +1,8 @@
 // 휴대폰 번호 형식 변경 함수
 export default function formatPhoneNumber(phoneNumber) {
-  const lastPart = phoneNumber.length - 4;
-  return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3, lastPart)}-${phoneNumber.slice(lastPart)}`;
+  if (typeof phoneNumber === "string") {
+    const lastPart = phoneNumber.length - 4;
+    return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3, lastPart)}-${phoneNumber.slice(lastPart)}`;
+  }
+  return phoneNumber;
 }
