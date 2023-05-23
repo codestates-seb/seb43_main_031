06103ef9a6f1ff5a631f70121ddcd1369d99c2b9
dakyungoh <dakyung.work@gmail.com>
2023-05-23 18:13:00 +0900

    style : 미디어쿼리 사용해 스타일 변경

--- front-end/src/features/boards/WriteButtonArea.jsx
+++ front-end/src/features/boards/WriteButtonArea.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-function WriteButtonArea({ onClickWriteBoard }) {
+function WriteButtonArea({ onClickWriteBoard, onClickAllList }) {
   return (
     <div className="write-button-area">
+      <button type="button" className="write-button " onClick={onClickAllList}>
+        글 전체보기
+      </button>
       <button type="button" className="write-button" onClick={onClickWriteBoard}>
         글 작성하기
       </button>
