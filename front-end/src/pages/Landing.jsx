2474af4ce8b3c34b6f0a5265b4336144f3195569
Suam Kang <ksa199653@gmail.com>
2023-05-24 10:22:05 +0900

    fix : 랜딩페이지 합치기

--- front-end/src/pages/Landing.jsx
+++ front-end/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 import { useInView } from "react-intersection-observer";
 
@@ -9,6 +9,24 @@ import chatMobile from "../img/chat-mobile.png";
 import detailMobile from "../img/detail-mobile.png";
 import boardLaptop from "../img/list-laptop.png";
 
+// 공통 섹션 프레임
+const SharedSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  font-size: 24px;
+`;
+
+// 공통 텍스트 래퍼
+const SpanWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  color: black;
+`;
+
+// 에니메이션 페이드인
 const fadeIn = keyframes`
   from {
     opacity: 0;
@@ -20,7 +38,8 @@ const fadeIn = keyframes`
   }
 `;
 
-const AnimatedSDelay = styled.span`
+// 에니메이션 딜레이
+const FadeInDelay = styled.span`
   animation: ${fadeIn} 1s ease-in-out;
   animation-fill-mode: both;
   &:nth-child(1) {
@@ -31,44 +50,126 @@ const AnimatedSDelay = styled.span`
   }
 `;
 
-const SharedSection = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80vh;
-  font-size: 24px;
+// 에니메이션 투명도
+const opcityIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 `;
 
+// 에니메이션 투명도 딜레이
+const OpcityInDelay = styled.div`
+  animation: ${opcityIn} 1s ease-in-out;
+  animation-fill-mode: both;
+  &:nth-child(1) {
+    animation-delay: 0.3s;
+  }
+  &:nth-child(2) {
+    animation-delay: 1.1s;
+  }
+  &:nth-child(3) {
+    animation-delay: 1.9s;
+  }
+`;
+
+// 첫번째 페이지
 const SectionOne = styled(SharedSection)`
   background-color: #fae7e7;
-  span {
-    font-size: 3rem;
-    color: #743e3e;
-    font-weight: 700;
-    text-shadow: #12000358 0px 4px 4px;
-    opacity: 0;
-    transition: all 3s;
-    .hightLight {
-      display: inline;
-      color: rgb(208, 68, 71);
-    }
-  }
+  display: flex;
+  gap: 5rem;
   img {
     margin-top: 3rem;
     height: 25rem;
     width: 25rem;
     border-radius: 20px;
     box-shadow: #12000358 0px 4px 8px;
+    @media (max-width: 450px) {
+      height: 15rem;
+      width: 15rem;
+    }
+  }
+  @media (max-width: 450px) {
+    flex-direction: column;
   }
 `;
 
-const SpanWrapper = styled.div`
+// 첫번째 페이지 안 버튼 레퍼
+const ButtonWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  color: black;
+  justify-content: center;
+  & button {
+    height: 3rem;
+    width: 13rem;
+    margin-top: 5rem;
+    font-size: 1.5rem;
+    color: #fff;
+    font-weight: 600;
+    border-style: none;
+    border-radius: 20px;
+    background-color: rgb(208, 68, 71);
+    &:hover {
+      background-color: var(--primary-color);
+    }
+    @media (max-width: 450px) {
+      margin-top: 3rem;
+    }
+  }
 `;
 
+// 첫번째 페이지 안 텍스트 레퍼
+const SpanOneWrapper = styled(SpanWrapper)`
+  margin-top: 3rem;
+  color: #743e3e;
+  font-weight: 700;
+  @media (max-width: 450px) {
+    margin-top: 0;
+    text-align: center;
+  }
+  & span {
+    font-size: 2.5rem;
+    text-shadow: #12000358 0px 4px 4px;
+    transition: all 3s;
+    .hightLight {
+      display: inline;
+      color: rgb(208, 68, 71);
+    }
+  }
+  .subTitle {
+    font-size: 1.5rem;
+    margin-top: 2.5rem;
+    @media (max-width: 450px) {
+      margin-top: 1rem;
+      text-align: center;
+    }
+  }
+`;
+
+// 두번째 페이지
+const SectionTwo = styled(SharedSection)`
+  background-color: #db8787;
+  flex-direction: row;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    .chat-mobile {
+      width: 200px;
+      height: 250px;
+      margin: 1rem 5rem 0rem -2rem;
+    }
+    .detail-mobile {
+      width: 200px;
+      height: 250px;
+      position: absolute;
+      top: 49px;
+      left: 69px;
+      z-index: 1;
+    }
+  }
+`;
+
+// 두번째 페이지 안 텍스트 레퍼
 const SpanTwoWrapper = styled(SpanWrapper)`
   color: #fff;
   font-size: 2.5rem;
@@ -87,6 +188,7 @@ const SpanTwoWrapper = styled(SpanWrapper)`
   }
 `;
 
+// 두번째 페이지 안 모바일 사진
 const ImgChatMobile = styled.div`
   max-width: 100%;
   max-height: 100%;
@@ -112,27 +214,7 @@ const ImgChatMobile = styled.div`
   }
 `;
 
-const SectionTwo = styled(SharedSection)`
-  background-color: #db8787;
-  flex-direction: row;
-  @media (max-width: 600px) {
-    flex-direction: column;
-    .chat-mobile {
-      width: 200px;
-      height: 250px;
-      margin: 1rem 5rem 0rem -2rem;
-    }
-    .detail-mobile {
-      width: 200px;
-      height: 250px;
-      position: absolute;
-      top: 49px;
-      left: 69px;
-      z-index: 1;
-    }
-  }
-`;
-
+// 세번째 페이지
 const SectionThree = styled(SharedSection)`
   width: 100%;
   height: 900px;
@@ -143,71 +225,12 @@ const SectionThree = styled(SharedSection)`
   flex-direction: column;
 `;
 
+// 세번째 페이지 안 텍스트 래퍼
 const SpanThreeWrapper = styled(SpanWrapper)`
   display: flex;
 `;
 
-export default function Landing() {
-  const sectionOneRef = useRef(null);
-  const sectionTwoRef = useRef(null);
-
-  useEffect(() => {
-    const observerOne = new IntersectionObserver(entries => {
-      const { target } = entries[0];
-      if (entries[0].isIntersecting) {
-        target.style.opacity = 1;
-      }
-      if (!entries[0].isIntersecting) {
-        target.style.opacity = 0;
-      }
-    });
-
-    observerOne.observe(sectionOneRef.current);
-  }, []);
-
-  return (
-    <>
-      <SectionOne ref={sectionOneRef}>
-        <SpanWrapper>
-          <AnimatedSDelay>
-            우리동네 <p className="hightLight">심부름</p>은
-          </AnimatedSDelay>
-          <AnimatedSDelay>빨간망토에서</AnimatedSDelay>
-          <img src={mapClick} alt="map" />
-        </SpanWrapper>
-      </SectionOne>
-      <SectionTwo ref={sectionTwoRef}>
-        <AnimatedSDelay>
-          <SpanTwoWrapper>
-            <span>1:1 채팅으로</span>
-            <span>간편하고 빠르게 매칭</span>
-            <span className="sub-content">연결된 사람과 원활히 소통하여 윈윈할 수 있는 활동을 할 수 있어요!</span>
-          </SpanTwoWrapper>
-        </AnimatedSDelay>
-        <ImgChatMobile>
-          <img src={detailMobile} alt="detail-mobile" className="detail-mobile" />
-          <img src={chatMobile} alt="chat-mobile" className="chat-mobile" />
-        </ImgChatMobile>
-      </SectionTwo>
-      <ObserverPattern>
-        <SpanThreeWrapper>
-          <AnimatedSDelaySecond>
-            <p>이웃과 함께하는 동네 생활</p>
-          </AnimatedSDelaySecond>
-          <AnimatedSDelaySecond>
-            <span>우리 동네 다양한 이야기를 함께 나눠요</span>
-          </AnimatedSDelaySecond>
-        </SpanThreeWrapper>
-        <ImgBoardLaptop>
-          <img src={boardLaptop} alt="board-laptop" />
-        </ImgBoardLaptop>
-        <GoToLogin type="button">소통하러 가기</GoToLogin>
-      </ObserverPattern>
-      <Footer />
-    </>
-  );
-}
-
+// 세번째 페이지 안 랩탑 사진
 const ImgBoardLaptop = styled.div`
   margin: 0;
   position: relative;
@@ -222,6 +245,7 @@ const ImgBoardLaptop = styled.div`
   }
 `;
 
+// 세번째 페이지 안 로그인 이동 버튼
 const GoToLogin = styled.button`
   padding: 1rem;
   background-color: var(--primary-color);
@@ -236,6 +260,62 @@ const GoToLogin = styled.button`
     opacity: 0.8;
   }
 `;
+
+export default function Landing() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <SectionOne>
+        <img src={mapClick} alt="map" />
+        <SpanOneWrapper>
+          <FadeInDelay>
+            우리 동네 <p className="hightLight">심부름</p>은
+          </FadeInDelay>
+          <FadeInDelay>빨간망토에서</FadeInDelay>
+          <OpcityInDelay>
+            <p className="subTitle">가까운 거리로 부담 없이 이용해 보세요!</p>
+            <ButtonWrapper>
+              <button type="button" onClick={() => navigate("/boards")}>
+                도와주러 가기
+              </button>
+            </ButtonWrapper>
+          </OpcityInDelay>
+        </SpanOneWrapper>
+      </SectionOne>
+      <ObserverPatternSecond>
+        <FadeInDelay>
+          <SpanTwoWrapper>
+            <span>1:1 채팅으로</span>
+            <span>간편하고 빠르게 매칭</span>
+            <span className="sub-content">연결된 사람과 원활히 소통하여 윈윈할 수 있는 활동을 할 수 있어요!</span>
+          </SpanTwoWrapper>
+        </FadeInDelay>
+        <ImgChatMobile>
+          <img src={detailMobile} alt="detail-mobile" className="detail-mobile" />
+          <img src={chatMobile} alt="chat-mobile" className="chat-mobile" />
+        </ImgChatMobile>
+      </ObserverPatternSecond>
+      <ObserverPattern>
+        <SpanThreeWrapper>
+          <AnimatedSDelaySecond>
+            <p>이웃과 함께하는 동네 생활</p>
+          </AnimatedSDelaySecond>
+          <AnimatedSDelaySecond>
+            <span>우리 동네 다양한 이야기를 함께 나눠요</span>
+          </AnimatedSDelaySecond>
+        </SpanThreeWrapper>
+        <ImgBoardLaptop>
+          <img src={boardLaptop} alt="board-laptop" />
+        </ImgBoardLaptop>
+        <GoToLogin type="button" onClick={() => navigate("/login")}>
+          소통하러 가기
+        </GoToLogin>
+      </ObserverPattern>
+      <Footer />
+    </>
+  );
+}
 
 const AnimatedSDelaySecond = styled.span`
   animation: ${fadeIn} 1s ease-in-out;
@@ -275,5 +355,23 @@ function ObserverPattern({ children }) {
     >
       {children}
     </SectionThree>
+  );
+}
+
+function ObserverPatternSecond({ children }) {
+  const { ref, inView } = useInView({
+    threshold: 0.7,
+  });
+
+  return (
+    <SectionTwo
+      ref={ref}
+      style={{
+        transition: "all 1s ease-in-out",
+        opacity: inView ? 1 : 0,
+      }}
+    >
+      {children}
+    </SectionTwo>
   );
 }
